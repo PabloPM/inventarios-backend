794e8a346adb5534b063f43dff25448d428ca16a
PabloPM <pa.ma.pa@hotmail.com>
2024-10-17 05:52:32 -0600

    ajustes

--- index.js
+++ index.js
@@ -22,21 +22,9 @@ app.post('/api/notification', (req, res) => {
   for (const token of tokens) {
     admin.messaging().send({
       token: token, // ['token_1', 'token_2', ...]
-      data:{
-        notifee: {
-          body: `${sales} solicita ${productName}`,
-          android: {
-            channelId: 'default',
-            actions: [
-              {
-                title: 'Mark as Read',
-                pressAction: {
-                  id: 'read',
-                },
-              },
-            ],
-          }
-        } 
+      data: {
+        title: `Pedido de ${sales}`,
+        body: `${sales} solicita ${productName}`,
       },
       notification: {
         title: `Pedido de ${sales}`,
