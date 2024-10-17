2374bd4b9cfcff45369d25055e92f570c3438e11
PabloPM <pa.ma.pa@hotmail.com>
2024-10-16 19:56:48 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const admin = require("firebase-admin");
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8082; 
+const config = require('/etc/secrets/config.json')
+
+app.use(express.json())
+
+admin.initializeApp({
+  credential: admin.credential.cert(config)
+});
+
+app.get('/', (req, res) => {
+  res.send(':D')
+})
+
+app.post('/api/notification', (req, res) => {
+  const { tokens=[], productName, store } = req.body
+  console.log(req.body)
+
+  for (const token of tokens) {
+    admin.messaging().send({
+      token: token, // ['token_1', 'token_2', ...]
+      notification: {
+        title: 'Basic Notification',
+        body: 'This is a basic notification sent from the server!',
+        imageUrl: 'https://my-cdn.com/app-logo.png',
+      },
+    })
+  }
+
+  res.json({'hello': 'world' })
+});
+
+app.listen(PORT)
+
+
+console.log(':D')
