3f88ec937aed5199d18e87010dab431581500f95
PabloPM <pa.ma.pa@hotmail.com>
2024-10-16 20:12:31 -0600

    ajustes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 8082; 
-const config = require('/etc/secrets/config')
+const config = require('/etc/secrets/config.json')
 
 app.use(express.json())
 
